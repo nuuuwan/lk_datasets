df0e287e7f07d271a7010ba0a2f16c263da71e0f
nuuuwan <nuuuwan@gmail.com>
2025-10-05 14:22:42 +0530

    lint

--- src/lk_datasets/__init__.py
+++ src/lk_datasets/__init__.py
@@ -1,5 +1,5 @@
 # lk_datasets (auto generate by build_inits.py)
 # flake8: noqa: F408
 
-from lk_datasets.ArXivDoc import ArXivDoc
+from lk_datasets.arxiv_doc import ArXivDoc
 from lk_datasets.LKDatasetsGlobalReadMe import LKDatasetsGlobalReadMe
---
+++ src/lk_datasets/arxiv_doc/__init__.py
@@ -0,0 +1,4 @@
+# lk_datasets.arxiv_doc (auto generate by build_inits.py)
+# flake8: noqa: F408
+
+from lk_datasets.arxiv_doc.ArXivDoc import ArXivDoc
