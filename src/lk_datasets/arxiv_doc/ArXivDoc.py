18d6ae1c65429d70d621372482f076944d354d97
nuuuwan <nuuuwan@gmail.com>
2025-10-08 14:26:12 +0530

    Added bbl to no cleanup

--- src/lk_datasets/arxiv_doc/ArXivDoc.py
+++ src/lk_datasets/arxiv_doc/ArXivDoc.py
@@ -6,20 +6,27 @@ from utils import File, Log, Time, TimeFormat
 
 from lk_datasets.arxiv_doc.ArXivDocEnd import ArXivDocEnd
 from lk_datasets.arxiv_doc.ArXivDocPreamble import ArXivDocPreamble
-from lk_datasets.arxiv_doc.ArXivDocSection0Abstract import \
-    ArXivDocSection0Abstract
-from lk_datasets.arxiv_doc.ArXivDocSection1Introduction import \
-    ArXivDocSection1Introduction
-from lk_datasets.arxiv_doc.ArXivDocSection2RelatedWork import \
-    ArXivDocSection2RelatedWork
-from lk_datasets.arxiv_doc.ArXivDocSection3Datasets import \
-    ArXivDocSection3Datasets
-from lk_datasets.arxiv_doc.ArXivDocSection4DataCollectionPipeline import \
-    ArXivDocSection4DataCollectionPipeline
-from lk_datasets.arxiv_doc.ArXivDocSection5LicensingAndAccess import \
-    ArXivDocSection5LicensingAndAccess
-from lk_datasets.arxiv_doc.ArXivDocSection6ConclusionAndFutureWork import \
-    ArXivDocSection6ConclusionAndFutureWork
+from lk_datasets.arxiv_doc.ArXivDocSection0Abstract import (
+    ArXivDocSection0Abstract,
+)
+from lk_datasets.arxiv_doc.ArXivDocSection1Introduction import (
+    ArXivDocSection1Introduction,
+)
+from lk_datasets.arxiv_doc.ArXivDocSection2RelatedWork import (
+    ArXivDocSection2RelatedWork,
+)
+from lk_datasets.arxiv_doc.ArXivDocSection3Datasets import (
+    ArXivDocSection3Datasets,
+)
+from lk_datasets.arxiv_doc.ArXivDocSection4DataCollectionPipeline import (
+    ArXivDocSection4DataCollectionPipeline,
+)
+from lk_datasets.arxiv_doc.ArXivDocSection5LicensingAndAccess import (
+    ArXivDocSection5LicensingAndAccess,
+)
+from lk_datasets.arxiv_doc.ArXivDocSection6ConclusionAndFutureWork import (
+    ArXivDocSection6ConclusionAndFutureWork,
+)
 from lk_datasets.LKDatasetsGlobalReadMe import LKDatasetsGlobalReadMe
 
 log = Log("ArXivDoc")
@@ -55,6 +62,7 @@ class ArXivDoc(
                 ".tex",
                 ".pdf",
                 ".bib",
+                ".bbl",
             ]:
                 path = os.path.join(self.DIR_LATEX, file)
                 log.debug(f"Removing old file {File(path)}")
