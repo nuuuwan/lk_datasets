d91e0087b2e1f44c315819ba93c03d9de6ae1afa
Nuwan I. Senaratna <nuuuwan@gmail.com>
2025-10-14 10:10:03 +0530

    Update LKDatasetsGlobalReadMe.py

--- src/lk_datasets/LKDatasetsGlobalReadMe.py
+++ src/lk_datasets/LKDatasetsGlobalReadMe.py
@@ -34,6 +34,7 @@ class LKDatasetsGlobalReadMe(GlobalReadMe):
                 "cbsl": ["cbsl_annual_reports"],
                 "lk_fisheries": [
                     "lk_fisheries_annual_statistics_reports",
+                    "lk_fisheries_monthly_fish_production_reports",
                 ],
                 "lk_edupub": ["lk_edupub"],
             }
